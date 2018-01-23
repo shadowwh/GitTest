7e39bdacdfa55e4faf2a7103f1819e85b42dbe9a
shadow.guo <shadowgzq@gmail.com>
2018-01-23 17:28:57 +0800

    dev branch 修改

--- src/main/java/com/shadow/git/test/BaseInfo.java
+++ src/main/java/com/shadow/git/test/BaseInfo.java
@@ -1,5 +1,13 @@
 package com.shadow.git.test;
 
 public class BaseInfo {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
 
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+	}
 }
